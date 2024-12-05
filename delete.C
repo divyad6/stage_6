11662e088da24f27d848a8a588ea3db31b0b5f2e
Divya Durgavarjhula <53246583+divyad6@users.noreply.github.com>
2024-12-05 16:49:13 -0600

    update delete.C

--- delete.C
+++ delete.C
@@ -1,5 +1,6 @@
 #include "catalog.h"
 #include "query.h"
+#include "heapfile.h"
 
 
 /*
@@ -16,10 +17,35 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
+	// part 6
 
+	// try to get attribute descriptor
+	AttrDesc attrDesc; 
+	Status status = attrCat->getInfo(relation, attrName, attrDesc);
+    	if (status != OK) {
+    	    return status;  
+    	}
 
+	// do a filtered scan to find which records to delete
+	HeapFileScan scan(relation, status);
+	if (status != OK) {
+    	    return status;  
+    	}
+	scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, attrValue, op);
+
+	// iterate through scan and delete the matching records
+	RID rid; 
+	while (scan.scanNext(rid) == OK) {
+		status = scan.deleteRecord();
+		if (status != OK) { // problem when trying to delete record
+			scan.endScan();
+			return status;
+		}
+	}
+
+	scan.endScan();
+
+	return OK;
 
 }
 
