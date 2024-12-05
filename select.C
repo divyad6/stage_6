673e2764ab00db78820c118def7567316a40a03c
Jenovesan <JameKelly1257@gmail.com>
2024-12-05 16:46:48 -0600

    select 1

--- select.C
+++ select.C
@@ -29,6 +29,28 @@ const Status QU_Select(const string & result,
    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
 
+    AttrDesc attrDescArray[projCnt];
+    for (int i = 0; i < projCnt; i++)
+    {
+        Status status = attrCat->getInfo(projNames[i].relName,
+                                         projNames[i].attrName,
+                                         attrDescArray[i]);
+        if (status != OK)
+        {
+            return status;
+        }
+    }
+
+    AttrDesc attrDesc;
+    attrCat->getInfo(attr->relName, attr->attrName, attrDesc);
+
+    int reclen = 0;
+    for (int i = 0; i < projCnt; i++)
+    {
+        reclen += attrDescArray[i].attrLen;
+    }
+
+    ScanSelect(result, projCnt, attrDescArray, &attrDesc, op, attrValue, reclen)
 }
 
 
@@ -44,5 +66,66 @@ const Status ScanSelect(const string & result,
 {
     cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
 
+    Status status;
 
+    InsertFileScan resultRel(result, status);
+    if (status != OK)
+    {
+        std::cout << "failed 1" << std::endl;
+        return status;
+    }
+
+    char outputData[reclen];
+    Record outputRec;
+    outputRec.data = (void *) outputData;
+    outputRec.length = reclen;
+
+    HeapFileScan scan(attrDesc->relName, status);
+    if (status != OK)
+    {
+        std::cout << "failed 2" << std::endl;
+        return status;
+    }
+
+    status = scan.startScan(attrDesc->attrOffset,
+                                 attrDesc->attrLen,
+                                 (Datatype) attrDesc->attrType,
+                                 filter,
+                                 op);
+    if (status != OK)
+    {
+        std::cout << "failed 3" << std::endl;
+        return status;
+    }
+
+    RID rid;
+    while (scan.scanNext(rid) == OK)
+    {
+        Record record;
+        status = scan.getRecord(record);
+        if (status != OK)
+        {
+            std::cout << "failed 4" << std::endl;
+            return status;
+        }
+
+        // we have a match, copy data into the output record
+        int outputOffset = 0;
+        for (int i = 0; i < projCnt; i++)
+        {
+            memcpy(outputData + outputOffset, (char*)record.data + projNames[i].attrOffset, projNames[i].attrLen);
+
+            outputOffset += projNames[i].attrLen;
+        } // end copy attrs
+
+        // add the new record to the output relation
+        RID outRID;
+        status = resultRel.insertRecord(outputRec, outRID);
+        if (status != OK)
+        {
+            std::cout << "failed 5" << std::endl;
+            return status;
+        }
+    }
+    return OK;
 }
