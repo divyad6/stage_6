a25d5cf8a5d66dd444d7f08dea91a0ad186fdadb
Divya Durgavarjhula <divya@Divyas-MacBook-Pro.local>
2024-12-06 13:51:28 -0600

    reset select file for this branch

--- select.C
+++ select.C
@@ -29,52 +29,6 @@ const Status QU_Select(const string & result,
    // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
 
-    AttrDesc attrDescArray[projCnt];
-    for (int i = 0; i < projCnt; i++)
-    {
-        std::cout << "Getting attrDescArray " << i << std::endl;
-        Status status = attrCat->getInfo(projNames[i].relName,
-                                         projNames[i].attrName,
-                                         attrDescArray[i]);
-        if (status != OK)
-        {
-            std::cout << "failed 0" << std::endl;
-            return status;
-        }
-    }
-
-    std::cout << "Getting attrDesc" << std::endl;
-
-    AttrDesc attrDesc;
-    Operator our_operator = op;
-    if (attr != nullptr)
-    {
-        Status status = attrCat->getInfo(attr->relName, attr->attrName, attrDesc);
-        if (status != OK)
-        {
-            std::cout << "failed 0.5" << std::endl;
-            return status;
-        }
-    }
-    else
-    {
-         our_operator = EQ;
-         attrValue = nullptr;
-    }
-
-    std::cout << "Getting reclen" << std::endl;
-
-    int reclen = 0;
-    for (int i = 0; i < projCnt; i++)
-    {
-        reclen += attrDescArray[i].attrLen;
-    }
-
-    std::cout << "Calling ScanSelect" << std::endl;
-
-    ScanSelect(result, projCnt, attrDescArray, &attrDesc, our_operator, attrValue, reclen);
-
-    std::cout << "Called ScanSelect" << std::endl;
 }
 
 
@@ -90,67 +44,5 @@ const Status ScanSelect(const string & result,
 {
     cout << "Doing HeapFileScan Selection using ScanSelect()" << endl;
 
-    Status status;
 
-    InsertFileScan resultRel(result, status);
-    if (status != OK)
-    {
-        std::cout << "failed 1" << std::endl;
-        return status;
-    }
-
-    char outputData[reclen];
-    Record outputRec;
-    outputRec.data = (void *) outputData;
-    outputRec.length = reclen;
-
-    std::cout << "name " << attrDesc->relName << std::endl;
-    HeapFileScan scan(string(attrDesc->relName), status);
-    if (status != OK)
-    {
-        std::cout << "failed 2" << std::endl;
-        return status;
-    }
-
-    status = scan.startScan(attrDesc->attrOffset,
-                                 attrDesc->attrLen,
-                                 (Datatype) attrDesc->attrType,
-                                 filter,
-                                 op);
-    if (status != OK)
-    {
-        std::cout << "failed 3" << std::endl;
-        return status;
-    }
-
-    RID rid;
-    while (scan.scanNext(rid) == OK)
-    {
-        Record record;
-        status = scan.getRecord(record);
-        if (status != OK)
-        {
-            std::cout << "failed 4" << std::endl;
-            return status;
-        }
-
-        // we have a match, copy data into the output record
-        int outputOffset = 0;
-        for (int i = 0; i < projCnt; i++)
-        {
-            memcpy(outputData + outputOffset, (char*)record.data + projNames[i].attrOffset, projNames[i].attrLen);
-
-            outputOffset += projNames[i].attrLen;
-        } // end copy attrs
-
-        // add the new record to the output relation
-        RID outRID;
-        status = resultRel.insertRecord(outputRec, outRID);
-        if (status != OK)
-        {
-            std::cout << "failed 5" << std::endl;
-            return status;
-        }
-    }
-    return OK;
 }
