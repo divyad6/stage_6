d83dc65e332afee3c772a42c6cf8f36d7f9aa9e2
Divya Durgavarjhula <divya@Divyas-MacBook-Pro.local>
2024-12-06 13:43:43 -0600

    added type switch statement and NULL checks to delete

--- delete.C
+++ delete.C
@@ -17,24 +17,60 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-	// part 6
+	std::cout << "Doing QU_Delete\n";
+
+	Status status;
 
 	// try to get attribute descriptor
 	AttrDesc attrDesc; 
-	Status status = attrCat->getInfo(relation, attrName, attrDesc);
+	if (attrValue != nullptr) {
+		status = attrCat->getInfo(relation, attrName, attrDesc);
     	if (status != OK) {
     	    return status;  
     	}
+	}
+
+	// get relation info
+	RelDesc relDesc;
+	status = relCat->getInfo(relation, relDesc);
+	if (status != OK) {
+		return status;
+	}
 
 	// do a filtered scan to find which records to delete
 	HeapFileScan scan(relation, status);
-	if (status != OK) {
-    	    return status;  
-    	}
-	scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, attrValue, op);
+
+	int i;
+	float f;
+
+	if (attrValue == NULL) {
+		scan.startScan(0, 0, STRING, NULL, EQ);
+	} else {
+		// convert attrValue to correct type and start scan
+		switch(type) {
+			char* value;
+			case INTEGER:
+				value = (char *)attrValue;
+				i = atoi(value);
+				status = scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, (char *)&i, op);
+				break;
+			case FLOAT:
+				value = (char *)attrValue;
+				f = atof(value);
+				status = scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, (char *)&f, op);
+				break;
+			default:
+				scan.startScan(attrDesc.attrOffset, attrDesc.attrLen, type, attrValue, op);
+		}
+
+		if (status != OK) {
+			return status;
+		}
+	}
 
 	// iterate through scan and delete the matching records
 	RID rid; 
+	Record rec;
 	while (scan.scanNext(rid) == OK) {
 		status = scan.deleteRecord();
 		if (status != OK) { // problem when trying to delete record
